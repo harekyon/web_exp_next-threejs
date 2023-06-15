90b997779c5d92c43e1ddafc2b88fad344621a4d
harekyon <harekyon@gmail.com>
2023-06-15 19:15:42 +0900

    部屋造った

--- app/page.js
+++ app/page.js
@@ -3,20 +3,21 @@
 import Image from "next/image";
 import { useEffect } from "react";
 import * as THREE from "three";
+import { createMultiMaterialObject } from "../node_modules/three/examples/jsm/utils/SceneUtils.js";
 import * as Stats from "stats-js";
 import * as dat from "dat.gui";
+// import img from "../";
 
 export default function Home() {
-  // let canvas = null;
   useEffect(() => {
     // if (canvas) return;
     // canvasを取得
     let stats = initStats();
     let scene = new THREE.Scene();
     // scene.fog = new THREE.Fog(0xffffff, 0.015, 100);
-    scene.overrideMaterial = new THREE.MeshLambertMaterial({
-      color: 0xffffff,
-    });
+    // scene.overrideMaterial = new THREE.MeshLambertMaterial({
+    //   color: 0xffffff,
+    // });
 
     let camera = new THREE.PerspectiveCamera(
       45,
@@ -31,31 +32,112 @@ export default function Home() {
     camera.lookAt(scene.position);
 
     let renderer = new THREE.WebGLRenderer();
-    renderer.setClearColor(new THREE.Color(0xeeeeee));
+    renderer.setClearColor(new THREE.Color(0x010103));
     renderer.setSize(window.innerWidth, window.innerHeight);
     renderer.shadowMap.enable = true;
 
     document.getElementById("WebGL-output").appendChild(renderer.domElement);
 
+    let axes = new THREE.AxesHelper(25);
+    scene.add(axes);
+    let materials = [
+      new THREE.MeshLambertMaterial({
+        opacity: 0.6,
+        color: 0x44ff44,
+        transparent: true,
+      }),
+      new THREE.MeshBasicMaterial({ color: 0x000000, wireframe: true }),
+    ];
     let planeGeometry = new THREE.PlaneGeometry(50, 50, 1, 1);
-    let planeMaterial = new THREE.MeshLambertMaterial({ color: 0xffffff });
-    let plane = new THREE.Mesh(planeGeometry, planeMaterial);
-    plane.receiveShadow = true;
-    plane.rotation.x = -0.5 * Math.PI;
-    plane.position.x = 0;
-    plane.position.y = 0;
-    plane.position.z = 0;
-    scene.add(plane);
 
-    let ambientLight = new THREE.AmbientLight(0x0c0c0c);
-    // scene.add(ambientLight);
-    var spotLight = new THREE.SpotLight(0xffffff);
+    let textureLoader = new THREE.TextureLoader();
+    let floorTex = {
+      texDiff: textureLoader.load(
+        "/laminate_floor_03_2k/textures/laminate_floor_03_diff_2k.jpg"
+      ),
+      texAo: textureLoader.load(
+        "/laminate_floor_03_2k/textures/laminate_floor_03_ao_2k.jpg"
+      ),
+      texNormal: textureLoader.load(
+        "/laminate_floor_03_2k/textures/laminate_floor_03_nor_dx_2k.jpg"
+      ),
+      texDisp: textureLoader.load(
+        "/laminate_floor_03_2k/textures/laminate_floor_03_disp_2k.jpg"
+      ),
+      texRough: textureLoader.load(
+        "/laminate_floor_03_2k/textures/laminate_floor_03_rough_2k.jpg"
+      ),
+    };
+    console.log(floorTex.texDiff);
+    floorTex.texDiff.wrapS = THREE.RepeatWrapping;
+    floorTex.texDiff.wrapT = THREE.RepeatWrapping;
+    floorTex.texDiff.repeat = new THREE.Vector2(2, 2);
+    floorTex.texDiff.rotation = -0.5 * Math.PI;
+    let wallTex = {
+      texDiff: textureLoader.load(
+        "/leather_white_2k/textures/leather_white_diff_2k.jpg"
+      ),
+      texAo: textureLoader.load(
+        "/leather_white_2k/textures/leather_white_ao_2k.jpg"
+      ),
+      texNormal: textureLoader.load(
+        "/leather_white_2k/textures/leather_white_nor_gl_2k.jpg"
+      ),
+      texRough: textureLoader.load(
+        "/leather_white_2k/textures/leather_white_rough_2k.jpg"
+      ),
+    };
+    let floorMaterials = new THREE.MeshStandardMaterial({
+      map: floorTex.texDiff,
+      normalMap: floorTex.texNormal,
+      normalScale: new THREE.Vector2(1, -1),
+      aoMap: floorTex.texAo,
+      displacementMap: floorTex.texDisp,
+      roughnessMap: floorTex.texRough,
+    });
+    let wallMaterials = new THREE.MeshStandardMaterial({
+      map: wallTex.texDiff,
+      normalMap: wallTex.texNormal,
+      normalScale: new THREE.Vector2(1, -1),
+      aoMap: wallTex.texAo,
+      roughnessMap: wallTex.texRough,
+    });
+    let floor = new THREE.Mesh(planeGeometry, floorMaterials);
+    floor.receiveShadow = true;
+    floor.rotation.x = -0.5 * Math.PI;
+    floor.position.x = 0;
+    floor.position.y = 0;
+    floor.position.z = 0;
+    scene.add(floor);
+    let leftWall = new THREE.Mesh(planeGeometry, wallMaterials);
+    leftWall.receiveShadow = true;
+    leftWall.rotation.y = -0.5 * Math.PI;
+    leftWall.position.x = 25;
+    leftWall.position.y = 25;
+    leftWall.position.z = 0;
+    scene.add(leftWall);
+    let rightWall = new THREE.Mesh(planeGeometry, wallMaterials);
+    rightWall.receiveShadow = true;
+    rightWall.rotation.z = 0.5 * Math.PI;
+    rightWall.position.x = 0;
+    rightWall.position.y = 25;
+    rightWall.position.z = -25;
+
+    scene.add(rightWall);
+
+    let ambientLight = new THREE.AmbientLight(0x444444);
+    scene.add(ambientLight);
+    let spotLight = new THREE.SpotLight(0x555555);
     spotLight.position.set(-20, 30, -5);
     spotLight.castShadow = true;
     scene.add(spotLight);
+    let pointLight = new THREE.PointLight(0x444444);
+    spotLight.position.set(-20, 30, -5);
+    pointLight.castShadow = true;
+    scene.add(pointLight);
 
     let controls = new (function () {
-      this.rotationSpeed = 0.02;
+      this.rotationSpeed = 0.0;
     })();
 
     let gui = new dat.GUI();
@@ -67,7 +149,7 @@ export default function Home() {
       stats.update();
 
       scene.traverse(function (obj) {
-        if (obj instanceof THREE.Mesh && obj != plane) {
+        if (obj instanceof THREE.Mesh && obj != floor) {
           obj.rotation.x += controls.rotationSpeed;
           obj.rotation.y += controls.rotationSpeed;
           obj.rotation.z += controls.rotationSpeed;
@@ -89,7 +171,7 @@ export default function Home() {
       return stats;
     }
     window.addEventListener("resize", onResize, false);
-    console.log("aaa");
+
     function onResize() {
       camera.aspect = window.innerWidth / window.innerHeight;
       camera.updateProjectionMatrix();
