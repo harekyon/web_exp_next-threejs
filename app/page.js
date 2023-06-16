d5dc9a93f1d949de5625bdb5ff007b091f4e2271
harekyon <harekyon@gmail.com>
2023-06-16 11:07:04 +0900

    orbitcontrol追加。テクスチャの初期化関数追加

--- app/page.js
+++ app/page.js
@@ -1,15 +1,21 @@
 "use client";
 
+// basis
 import Image from "next/image";
 import { useEffect } from "react";
-import * as THREE from "three";
-import { createMultiMaterialObject } from "../node_modules/three/examples/jsm/utils/SceneUtils.js";
+
+// extension lib
 import * as Stats from "stats-js";
 import * as dat from "dat.gui";
-// import img from "../";
+
+// three lib
+import * as THREE from "three";
+import { createMultiMaterialObject } from "../node_modules/three/examples/jsm/utils/SceneUtils.js";
+import { OrbitControls } from "../node_modules/three/examples/jsm/controls/OrbitControls.js";
 
 export default function Home() {
   useEffect(() => {
+    console.log(OrbitControls);
     // if (canvas) return;
     // canvasを取得
     let stats = initStats();
@@ -38,6 +44,12 @@ export default function Home() {
 
     document.getElementById("WebGL-output").appendChild(renderer.domElement);
 
+    console.log(OrbitControls);
+    let orbitControls = new OrbitControls(camera, renderer.domElement);
+    // orbitControls.autoRotate = true;
+    let clock = new THREE.Clock();
+    let delta = clock.getDelta();
+
     let axes = new THREE.AxesHelper(25);
     scene.add(axes);
     let materials = [
@@ -53,7 +65,9 @@ export default function Home() {
     let textureLoader = new THREE.TextureLoader();
     let floorTex = {
       texDiff: textureLoader.load(
-        "/laminate_floor_03_2k/textures/laminate_floor_03_diff_2k.jpg"
+        "/laminate_floor_03_2k/textures/laminate_floor_03_diff_2k.jpg",
+        THREE.RepeatWrappin,
+        THREE.RepeatWrappin
       ),
       texAo: textureLoader.load(
         "/laminate_floor_03_2k/textures/laminate_floor_03_ao_2k.jpg"
@@ -73,6 +87,18 @@ export default function Home() {
     floorTex.texDiff.wrapT = THREE.RepeatWrapping;
     floorTex.texDiff.repeat = new THREE.Vector2(2, 2);
     floorTex.texDiff.rotation = -0.5 * Math.PI;
+    function autoInitTexture(textureObj) {
+      textureObj.wrapS = THREE.RepeatWrapping;
+      textureObj.wrapT = THREE.RepeatWrapping;
+      textureObj.repeat = new THREE.Vector2(2, 2);
+      textureObj.rotation = -0.5 * Math.PI;
+    }
+    autoInitTexture(floorTex.texDiff);
+    autoInitTexture(floorTex.texAo);
+    autoInitTexture(floorTex.texNormal);
+    autoInitTexture(floorTex.texDisp);
+    autoInitTexture(floorTex.texRough);
+
     let wallTex = {
       texDiff: textureLoader.load(
         "/leather_white_2k/textures/leather_white_diff_2k.jpg"
@@ -155,6 +181,7 @@ export default function Home() {
           obj.rotation.z += controls.rotationSpeed;
         }
       });
+      orbitControls.update(delta);
       requestAnimationFrame(render);
       renderer.render(scene, camera);
     }
